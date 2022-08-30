e406426b725f2f8df013d61a9c2c2796c278ad2d
m-lafauci <mglafauci@gmail.com>
2022-08-30 00:08:09 -0300

    Desafio: Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+
 
 function App() {
   
   return (
     <div className='container-fluid'>
       <NavBar />
+      <ItemListContainer />
+      
        
     </div>
   );
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItemListContainer = () => {
+    return (
+        <div>
+            <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, fugiat!</h2>
+        </div>
+    )
+}
+
+export default ItemListContainer;
+
+
